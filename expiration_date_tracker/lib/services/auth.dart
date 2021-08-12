205b00146af2644b18aabd213e1fea36c54e95b8
Gillian Evers <gillian.e2019@gmail.com>
2021-08-12 14:00:12 -0400

    Firebase Auth setup

---
+++ expiration_date_tracker/lib/services/auth.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+  Future signInAnon() async {
+    try {
+      UserCredential credential = await _auth.signInAnonymously();
+      User user = credential.user;
+      return user;
+    } catch (e) {
+      print(e.tostring());
+      return null;
+    }
+  }
+
+  // sign in phone
+
+  // register w phone
+
+  // sign out
+}
---
+++ expiration_date_tracker/lib/services/authenticate.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:expiration_date_tracker/screens/login.dart';
+import 'package:expiration_date_tracker/screens/register.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool viewLogin = true;
+  void toggleView() {
+    setState(() => viewLogin = !viewLogin);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (viewLogin) {
+      return LoginScreen(toggleView: toggleView);
+    } else {
+      return RegisterScreen(toggleView: toggleView);
+    }
+  }
+}
