981aa5f5b5cc43e3659777b7fb7b2715c90b80b2
Gillian Evers <gillian.e2019@gmail.com>
2021-08-12 13:59:15 -0400

    Created login and register screens

--- expiration_date_tracker/lib/screens/login.dart
+++ expiration_date_tracker/lib/screens/login.dart
@@ -1,3 +1,87 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  final Function toggleView;
+  LoginScreen({this.toggleView});
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  void initState() {
+    super.initState();
+  }
+
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    final codeField = TextFormField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Verification Code",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff952efc),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style:
+              style.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text('Log in'),
+          actions: <Widget>[
+            TextButton.icon(
+                onPressed: () {
+                  widget.toggleView();
+                },
+                icon: Icon(Icons.person),
+                label: Text('Register'),
+                style: TextButton.styleFrom(primary: Colors.black))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 45.0),
+                    codeField,
+                    SizedBox(height: 25.0),
+                    loginButton,
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
+
 // AppBar: Title, register button
 
 // TextFields: verification code
--- expiration_date_tracker/lib/screens/register.dart
+++ expiration_date_tracker/lib/screens/register.dart
@@ -1,3 +1,105 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  final Function toggleView;
+  RegisterScreen({this.toggleView});
+
+  @override
+  RegisterScreenState createState() => RegisterScreenState();
+}
+
+class RegisterScreenState extends State<RegisterScreen> {
+  void initState() {
+    super.initState();
+  }
+
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    final nameField = TextFormField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Enter name",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final phoneNumberField = TextFormField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Enter phone number",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final registerButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff952efc),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Register",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('Register'),
+        actions: <Widget>[
+          TextButton.icon(
+              onPressed: () {
+                widget.toggleView();
+              },
+              icon: Icon(Icons.person),
+              label: Text('Log in'),
+              style: TextButton.styleFrom(primary: Colors.black))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 45.0),
+                  nameField,
+                  SizedBox(height: 25.0),
+                  phoneNumberField,
+                  SizedBox(height: 35.0),
+                  registerButton,
+                  SizedBox(height: 35.0),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// AppBar: Title, register button
+
+// TextFields: verification code
+
+// Login button
+
+// Error: verification code invalid
+
 // AppBar: Register, Login button
 
 // TextFields: username, phone number
